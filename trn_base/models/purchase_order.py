5ab782365987e17023cada8b56b964054915221a
fabiandroguettdimabe <fabian.droguett@dimabe.cl>
2022-05-27 09:07:24 -0400

    add trn_base module

--- trn_base/models/purchase_order.py
+++ trn_base/models/purchase_order.py
@@ -1,8 +1,12 @@
 from odoo import fields, models, api
-
+from ..utils.roundformat_clp import round_clp
 
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
+    def roundclp(self, value):
+        return round_clp(value)
+
     def _get_custom_report_name(self):
         return '%s %s' % ('Órden de Compra - ', self.name)
+
