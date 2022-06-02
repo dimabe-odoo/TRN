a994b5f5223a780dec8f7556a6c8a53958039698
dimabe-odoo <desarrolloti@dimabe.cl>
2022-06-02 11:39:31 -0400

    add default note on purchase order

--- trn_base/models/purchase_order.py
+++ trn_base/models/purchase_order.py
@@ -1,3 +1,4 @@
+from venv import create
 from odoo import fields, models, api
 from ..utils.roundformat_clp import round_clp
 
@@ -9,4 +10,9 @@ class PurchaseOrder(models.Model):
 
     def _get_custom_report_name(self):
         return '%s %s' % ('Órden de Compra - ', self.name)
+
+    def create(self, values):
+        values['notes'] = 'Estimado proveedor, favor realizar el envío de facturas al correo: Recepcionfacturas@trn.cl'
+        res = super(PurchaseOrder, self).create(values)
+        return res
 
