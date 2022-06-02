a19540780da9db516bf4c295747bb256bcd9a617
dimabe-odoo <desarrolloti@dimabe.cl>
2022-06-02 18:14:33 -0400

    add unit and total cost by line of picking

--- trn_base/models/stock_move_line.py
+++ trn_base/models/stock_move_line.py
@@ -2,5 +2,15 @@ from odoo import fields, models, api
 
 class StockMoveLine(models.Model):
   _inherit = 'stock.move.line'
-  
+
   analytic_account = fields.Many2one('account.analytic.account', string = "Cuenta Analítica")
+  product_unit_cost = fields.Float('Costo Unitario')
+  product_total_cost = fields.Float('Costo Total')
+
+  def _action_done(self):
+    res = super(StockMoveLine, self)._action_done()
+    self.write({
+        'product_unit_cost': self.product_id.standard_price,
+        'product_total_cost': self.product_id.standard_price * self.qty_done
+    })
+    return res
