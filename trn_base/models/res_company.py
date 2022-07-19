10ef79411b8f888c23668908d07fe99edf0e9e8e
fabiandroguettdimabe <fabian.droguett@dimabe.cl>
2022-07-19 17:42:47 -0400

    add fields fix label

--- trn_base/models/res_company.py
+++ trn_base/models/res_company.py
@@ -5,6 +5,6 @@ class ResCompany(models.Model):
     _inherit = 'res.company'
 
     analytic_account_diff_id = fields.Many2one('account.analytic.account',
-                                                            string="Cuenta analitica de Diferencia en Valorización")
+                                                            string="Cuenta analítica de Diferencia en Valorización")
 
-    account_diff_id = fields.Many2one('account.account',stirng="Cuenta de diferencia de valorización")
+    account_diff_id = fields.Many2one('account.account',string="Cuenta de diferencia de valorización")
