17af64d4b9467bf6db14630185805c971098bef9
fabiandroguettdimabe <fabian.droguett@dimabe.cl>
2022-07-20 17:08:06 -0400

    add stock_min qty to product_template

--- trn_base/models/stock_quant.py
+++ trn_base/models/stock_quant.py
@@ -4,6 +4,17 @@ from odoo import models, fields, api
 class StockQuant(models.Model):
     _inherit = 'stock.quant'
 
+    @api.model
+    def create(self, vals_list):
+        res = super(StockQuant, self).create(vals_list)
+        if 'quantity' in vals_list.keys():
+            diff = res.product_id.product_tmpl_id.qty_available - res.product_id.product_tmpl_id.stock_min_qty
+            res.product_id.product_tmpl_id.write({
+                'stock_diff_qty': diff,
+                'state_stock': self.get_state_stock(diff)
+            })
+        return res
+
     def write(self, vals):
         res = super(StockQuant, self).write(vals)
         for item in self:
--- trn_base/models/stock_warehouse_orderpoint.py
+++ trn_base/models/stock_warehouse_orderpoint.py
@@ -12,3 +12,11 @@ class StockWarehouseOrderpoint(models.Model):
                 'stock_min_qty': res.product_min_qty
             })
         return res
+
+    def write(self, vals):
+        for item in self:
+            if 'product_min_qty' in vals.keys():
+                item.product_id.product_tmpl_id.write({
+                    'stock_min_qty': vals['product_min_qty']
+                })
+        return super(StockWarehouseOrderpoint, self).write(vals)
