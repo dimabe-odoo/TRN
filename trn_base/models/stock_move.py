d79525eca044a9552ff096dedb919300911e7dbc
Leonardo Norambuena <leonardonorambuena@MacBook-Pro-de-Leonardo.local>
2022-09-08 18:30:10 -0400

    fix error when debit is greater than credit

--- trn_base/models/stock_move.py
+++ trn_base/models/stock_move.py
@@ -22,17 +22,14 @@ class StockMove(models.Model):
                     res.append((0, 0, line_data))
                     total += line_data['debit']
             if res[0][2]['credit'] != total:
-                diff = abs(res[0][2]['credit'] - total)
-                account_type = 'credit'
-                if total > res[0][2]['credit']:
-                 account_type = 'debit'
-                line_diff_cost = self.get_data(1, 'Diferencia', account_type=account_type, account_id=self.env.company.account_diff_id.id if self.env.company.account_diff_id else debit_account_id,
+                diff = res[0][2]['credit'] - total
+                line_diff_cost = self.get_data(1, 'Diferencia', account_id=self.env.company.account_diff_id.id if self.env.company.account_diff_id else debit_account_id,
                                                analytic_account_id=self.env.company.analytic_account_diff_id.id,
                                                diff=diff)
                 res.append((0, 0, line_diff_cost))         
         return res
 
-    def get_data(self, qty, description, cost=False, account_id=False, analytic_account_id=False, diff=False, account_type=False):
+    def get_data(self, qty, description, cost=False, account_id=False, analytic_account_id=False, diff=False):
         data = {}
         data['name'] = description
         data['product_id'] = self.product_id.id
@@ -43,9 +40,9 @@ class StockMove(models.Model):
         if cost and not diff:
             data['debit'] = round(qty * cost) if self._is_out() else 0
             data['credit'] = round(qty * cost) if self._is_in() else 0
-        if not cost and diff and account_type:
-            data['debit'] = diff if account_type == 'debit' else 0
-            data['credit'] = diff if account_type == 'credit' else 0
+        if not cost and diff:
+            data['debit'] = abs(diff) if diff < 0 else 0
+            data['credit'] = abs(diff) if diff > 0 else 0
         data['account_id'] = account_id
         data['analytic_account_id'] = analytic_account_id
         data['currency_id'] = self.env.company.currency_id.id
