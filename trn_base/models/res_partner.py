339528b562a176d68a09cc51d3a38b88c68ca788
fabiandroguettdimabe <fabian.droguett@dimabe.cl>
2022-10-03 14:23:23 -0300

    add default none in massive charge res_partner

--- trn_base/models/res_partner.py
+++ trn_base/models/res_partner.py
@@ -1,8 +1,18 @@
 from odoo import fields, models, api
 
+
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     property_account_receivable_id = fields.Many2one('account.account', default=None, tracking=True)
 
     property_account_payable_id = fields.Many2one('account.account', default=None, tracking=True)
+
+    @api.model
+    def create(self, vals_list):
+        res = super(ResPartner, self).create(vals_list)
+        if res.property_account_receivable_id.deprecated:
+            res.property_account_receivable_id = None
+        if res.property_account_payable_id.deprecated:
+            res.property_account_payable_id = None
+        return res
